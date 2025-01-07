d4f892efabc338b23b8601a4559d7db9ebce89c2
Lars Schumacher <schumacher90lars@gmail.com>
2025-01-07 21:48:56 +0100

    merge resolved

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,10 @@
 import { Routes } from '@angular/router';
-<<<<<<< Updated upstream
-=======
 import { LoginComponent } from './login/login.component';
->>>>>>> Stashed changes
 import { HeaderComponent } from './header/header.component';
 import { SideNavBarComponent } from './side-nav-bar/side-nav-bar.component';
 
 export const routes: Routes = [
-<<<<<<< Updated upstream
-=======
     {path: 'login', component: LoginComponent},
->>>>>>> Stashed changes
     { path: 'header', component: HeaderComponent },
     { path: 'sidebar', component: SideNavBarComponent },
 ];
