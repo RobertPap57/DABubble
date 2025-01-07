108ac6f56ae9090f5bce50aab4c23997368e89e5
Alexander Hardtke <5yl3kz@googlemail.com>
2025-01-07 21:29:39 +0100

    router update for header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { SideNavBarComponent } from './side-nav-bar/side-nav-bar.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'home', component: HeaderComponent },
+    { path: 'sideBar', component: SideNavBarComponent },
+];
