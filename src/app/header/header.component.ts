697e59babdad832894112483e04bf55f33f7b304
Alexander Hardtke <5yl3kz@googlemail.com>
2025-01-08 13:11:22 +0100

    link creatUser

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { ProfileComponent } from './profile/profile.component';
 import { SearchbarComponent } from './searchbar/searchbar.component';
+import { LinkCreateComponent } from './link-create/link-create.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [ProfileComponent, SearchbarComponent],
+  imports: [ProfileComponent, SearchbarComponent, LinkCreateComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
---
+++ src/app/header/link-create/link-create.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-link-create',
+  standalone: true,
+  imports: [],
+  templateUrl: './link-create.component.html',
+  styleUrl: './link-create.component.scss'
+})
+export class LinkCreateComponent {
+
+}
