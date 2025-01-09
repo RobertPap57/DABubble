3c54da9eb1a68725c893f3ff5c59cbf275c80f09
Alexander Hardtke <5yl3kz@googlemail.com>
2025-01-09 15:22:25 +0100

    searchBar responsive

--- src/app/slide-side-bar/slide-side-bar.component.ts
+++ src/app/slide-side-bar/slide-side-bar.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { SideNavBarComponent } from './side-nav-bar/side-nav-bar.component';
+import { SearchbarComponent } from '../header/searchbar/searchbar.component';
 
 @Component({
   selector: 'app-slide-side-bar',
   standalone: true,
-  imports: [SideNavBarComponent],
+  imports: [SideNavBarComponent, SearchbarComponent],
   templateUrl: './slide-side-bar.component.html',
   styleUrl: './slide-side-bar.component.scss'
 })
