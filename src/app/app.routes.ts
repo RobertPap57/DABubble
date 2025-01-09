34c7f137db944d632b4099066ffba821deb6db6b
Lars Schumacher <schumacher90lars@gmail.com>
2025-01-09 08:46:38 +0100

    added create account route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { HeaderComponent } from './header/header.component';
 import { SideNavBarComponent } from './side-nav-bar/side-nav-bar.component';
+import { CreateAccountComponent } from './create-account/create-account.component';
 
 export const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'header', component: HeaderComponent },
   { path: 'sidebar', component: SideNavBarComponent },
+  { path: 'register', component: CreateAccountComponent },
 ];
