4ebeb33c4f772e9a886c389a0a68336b38c6957c
Alexander Hardtke <5yl3kz@googlemail.com>
2025-01-09 12:40:31 +0100

    slide in sidebar

--- src/app/slide-side-bar/slide-side-bar.component.ts
+++ src/app/slide-side-bar/slide-side-bar.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { SideNavBarComponent } from './side-nav-bar/side-nav-bar.component';
 
 @Component({
   selector: 'app-slide-side-bar',
   standalone: true,
-  imports: [],
+  imports: [SideNavBarComponent],
   templateUrl: './slide-side-bar.component.html',
   styleUrl: './slide-side-bar.component.scss'
 })
 export class SlideSideBarComponent {
+  sideNavOpen = true;
 
+  toggleSideBar() {
+    this.sideNavOpen = !this.sideNavOpen;
+  }
 }
