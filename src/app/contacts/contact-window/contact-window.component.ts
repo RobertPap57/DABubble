537e09eca49440c035b8497905a024af49d483b2
PaterAnderson <DariusMue@web.de>
2025-01-10 10:04:38 +0100

    added contact window style

--- src/app/contacts/contact-window/contact-window.component.ts
+++ src/app/contacts/contact-window/contact-window.component.ts
@@ -1,12 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-contact-window',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './contact-window.component.html',
   styleUrl: './contact-window.component.scss'
 })
 export class ContactWindowComponent {
-
 }
