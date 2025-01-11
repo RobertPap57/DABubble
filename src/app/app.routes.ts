1edbc0891df3ae75e3710cf7eed925ad06670881
Alexander Hardtke <5yl3kz@googlemail.com>
2025-01-11 21:33:24 +0100

    combined sidenav with home-screen

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,6 @@ import { LoginComponent } from './login/login.component';
 import { HeaderComponent } from './header/header.component';
 import { CreateAccountComponent } from './create-account/create-account.component';
 import { ChooseAvatarComponent } from './choose-avatar/choose-avatar.component';
-import { SlideSideBarComponent } from './slide-side-bar/slide-side-bar.component';
 import { ContactWindowComponent } from './contacts/contact-window/contact-window.component';
 import { PasswordResetSendMailComponent } from './password-reset-send-mail/password-reset-send-mail.component';
 import { PasswordResetComponent } from './password-reset/password-reset.component';
@@ -11,7 +10,6 @@ import { HomeComponent } from './home/home.component';
 export const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'header', component: HeaderComponent },
-  { path: 'sidebar', component: SlideSideBarComponent },
   { path: 'register', component: CreateAccountComponent },
   { path: 'avatar', component: ChooseAvatarComponent },
   { path: 'contact', component: ContactWindowComponent },
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ChatBoxComponent } from "../chat-box/chat-box.component";
+import { SlideSideBarComponent } from '../slide-side-bar/slide-side-bar.component';
 
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, ChatBoxComponent],
+  imports: [CommonModule, ChatBoxComponent, SlideSideBarComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
