12d9af1a74f8de47aff0c64a547e41bf75c95c8e
Alexander Hardtke <5yl3kz@googlemail.com>
2025-01-12 19:28:03 +0100

    implemented angularfire and service for user and priv msg

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,7 +4,10 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(), provideAnimationsAsync()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(), provideAnimationsAsync(), provideFirebaseApp(() => initializeApp({"projectId":"dabubble-389","appId":"1:878333384237:web:f57bf7bc4851545b424aa4","storageBucket":"dabubble-389.firebasestorage.app","apiKey":"AIzaSyA9xs0TpTyP712TjmTJF7ZgwjWVFY5ADBo","authDomain":"dabubble-389.firebaseapp.com","messagingSenderId":"878333384237"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())]
 };
