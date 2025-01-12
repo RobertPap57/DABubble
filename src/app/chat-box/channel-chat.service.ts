94aed274b4f620dba7715b2ebb749a84d5259a05
Florin-Robert Pap <165192808+RobertPap57@users.noreply.github.com>
2025-01-12 16:53:50 +0100

    Add hover effects for messages, implement emoji reactions - Display and count emoji reactions. - Add popup to show users who reacted with a specific emoji. - Count thread responses under messages and show the timestamp of the last response.

--- src/app/chat-box/channel-chat.service.ts
+++ src/app/chat-box/channel-chat.service.ts
@@ -9,10 +9,24 @@ export class ChannelChatService {
 
 
   getMessages(channelId: string) {
-    channelId = '123'; // replace with actual channel ID and get mesages from database
+    channelId = '123'; 
     return [
-      { sender: 'Alice', text: 'Hello!', time: '12:00', senderImg: '2' },
-      { sender: 'Bob', text: 'Hi there!', time: '13:00', senderImg: '1' }
+      {
+        sender: 'Alice',
+        text: 'Hello!',
+        time: '12:00',
+        senderImg: '2',
+        thread: [
+          { sender: 'Bob', text: 'Hi there!', time: '13:00', senderImg: '1' },
+          { sender: 'Alice', text: 'Hi there!', time: '15:00', senderImg: '2' }
+        ],
+        reactions: [
+          { emoji: '🚀', count: 3, users: ['Elena', 'Maria', 'Ionut'] },
+          { emoji: '✅', count: 1, users: ['Elena'] },
+
+        ],
+      },
+
     ];
   }
 }
--- src/app/chat-box/thread-chat.service.ts
+++ src/app/chat-box/thread-chat.service.ts
@@ -12,8 +12,22 @@ export class ThreadChatService {
   getMessages(channelId: string) {
     channelId = '123'; // replace with actual channel ID and get mesages from database
     return [
-      { sender: 'Alice', text: 'Hello!', time: '12:00', senderImg: '2' },
-      { sender: 'Bob', text: 'Hi there!', time: '13:00', senderImg: '1' }
-    ];
+      {
+        sender: 'Alice',
+        text: 'Hello!',
+        time: '12:00',
+        senderImg: '2',
+        thread: [
+          { sender: 'Bob', text: 'Hi there!', time: '13:00', senderImg: '1' },
+          { sender: 'Alice', text: 'Hi there!', time: '15:00', senderImg: '2' }
+        ],
+        reactions: [
+          { emoji: '🚀', count: 2, users: ['Elena', 'Maria'] },
+          { emoji: '✅', count: 1, users: ['Elena'] },
+
+        ],
+      },
+
+    ];;
   }
 }
