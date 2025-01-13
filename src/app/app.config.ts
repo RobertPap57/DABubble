3ba4842496d8b1a59905c73ff7b3a864483715ca
Lars Schumacher <schumacher90lars@gmail.com>
2025-01-13 09:33:01 +0100

    update after firebase addition

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -9,5 +9,22 @@ import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(), provideAnimationsAsync(), provideFirebaseApp(() => initializeApp({"projectId":"dabubble-389","appId":"1:878333384237:web:f57bf7bc4851545b424aa4","storageBucket":"dabubble-389.firebasestorage.app","apiKey":"AIzaSyA9xs0TpTyP712TjmTJF7ZgwjWVFY5ADBo","authDomain":"dabubble-389.firebaseapp.com","messagingSenderId":"878333384237"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideClientHydration(),
+    provideAnimationsAsync(),
+    provideFirebaseApp(() =>
+      initializeApp({
+        projectId: 'dabubble-389',
+        appId: '1:878333384237:web:f57bf7bc4851545b424aa4',
+        storageBucket: 'dabubble-389.firebasestorage.app',
+        apiKey: 'AIzaSyA9xs0TpTyP712TjmTJF7ZgwjWVFY5ADBo',
+        authDomain: 'dabubble-389.firebaseapp.com',
+        messagingSenderId: '878333384237',
+      })
+    ),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+  ],
 };
