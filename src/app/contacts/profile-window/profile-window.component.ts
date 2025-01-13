5175f7077af645e58de2517bfa26852a22eaca0c
PaterAnderson <DariusMue@web.de>
2025-01-13 11:27:02 +0100

    created profile-window component

---
+++ src/app/contacts/profile-window/profile-window.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-window',
+  standalone: true,
+  imports: [],
+  templateUrl: './profile-window.component.html',
+  styleUrl: './profile-window.component.scss'
+})
+export class ProfileWindowComponent {
+
+}
