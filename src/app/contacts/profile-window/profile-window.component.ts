d263866e90eebf6a7fed780f56bf2f1b88e5e3ff
PaterAnderson <DariusMue@web.de>
2025-01-14 13:55:44 +0100

    setting up profile window

--- src/app/contacts/profile-window/profile-window.component.ts
+++ src/app/contacts/profile-window/profile-window.component.ts
@@ -1,12 +1,24 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-profile-window',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './profile-window.component.html',
   styleUrl: './profile-window.component.scss'
 })
 export class ProfileWindowComponent {
+  name: string = 'Steffen Hoffmann';  // Beispielname
+  email: string = 'thehoffman@beispiel.com'; // Beispiel-E-Mail
+  isActive: boolean = false; // Status standardmäßig 'abwesend'
 
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  // Diese Methode könnte verwendet werden, um den Status von einer Datenbank abzurufen
+  fetchStatusFromDatabase() {
+  }
 }
