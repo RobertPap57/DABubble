c20e9bf5328c179da8c65b371d69d363022b9bc8
Alexander Hardtke <5yl3kz@googlemail.com>
2025-01-18 23:06:20 +0100

    responsive logoutslide

--- src/app/header/profile/profile.component.ts
+++ src/app/header/profile/profile.component.ts
@@ -1,4 +1,4 @@
-import { NgStyle } from '@angular/common';
+import { NgClass, NgStyle } from '@angular/common';
 import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
 import { UserService } from '../../services/user.service';
 import { UserIdService } from '../../services/user-id.service';
@@ -7,43 +7,48 @@ import { ContactWindowComponent } from '../../contacts/contact-window/contact-wi
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [NgStyle, ContactWindowComponent],
+  imports: [NgStyle, ContactWindowComponent, NgClass],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss'
 })
 export class ProfileComponent {
   @ViewChild('logoutBox') logoutBox!: ElementRef<HTMLDivElement>;
   @ViewChild('profileBox') profileBox!: ElementRef<HTMLDivElement>;
-  onlineColor:string = '#92c73e';
-  offlineColor:string = '#696969';
-  openLogoutBox = false;
-  openProfileBox = false;
+  onlineColor: string = '#92c73e';
+  offlineColor: string = '#696969';
+  openLogoutBox: boolean = false;
+  openProfileBox: boolean = false;
+  slideOut: boolean = false;
 
   constructor(public userIdService: UserIdService, public userService: UserService) { }
 
-    @HostListener('document:mouseup', ['$event.target'])
-    onClickOutsideLogoutBox(target: HTMLElement): void {
-      if (this.logoutBox) {
-        let clickInsideChan = this.logoutBox.nativeElement.contains(target); {
-        } if (!clickInsideChan) this.closeBoxes();
-      }
+  @HostListener('document:mouseup', ['$event.target'])
+  onClickOutsideLogoutBox(target: HTMLElement): void {
+    if (this.logoutBox) {
+      let clickInsideChan = this.logoutBox.nativeElement.contains(target); {
+      } if (!clickInsideChan) this.closeBoxes();
     }
+  }
 
-    @HostListener('document:click', ['$event.target'])
-    onClickOutsideProfileBox(target: HTMLElement): void {
-      if (this.profileBox) {
-        let clickInsideChan = this.profileBox.nativeElement.contains(target); {
-        } if (!clickInsideChan) this.closeBoxes();
-      }
+  @HostListener('document:click', ['$event.target'])
+  onClickOutsideProfileBox(target: HTMLElement): void {
+    if (this.profileBox) {
+      let clickInsideChan = this.profileBox.nativeElement.contains(target); {
+      } if (!clickInsideChan) this.closeBoxes();
     }
+  }
 
   openLogoutUser() {
     this.openLogoutBox = true;
+    this.slideOut = false;
   }
 
   closeBoxes() {
-    this.openLogoutBox = false;
     this.openProfileBox = false;
+    this.slideOut = true;
+    setTimeout(() => {
+      this.openLogoutBox = false;
+    }, 200);
   }
 
   openProfile(id: string) {
