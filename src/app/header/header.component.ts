ac5af596081d5e2766af9dfadde379577bfb2708
Alexander Hardtke <5yl3kz@googlemail.com>
2025-01-24 12:03:42 +0100

    ismobile durch CSS entfernt

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -20,27 +20,11 @@ export class HeaderComponent {
   isLoginRoute: boolean = false;
   isNotLoggedInRoute: boolean = false;
   isHomeRoute: boolean = false;
-  isMobile: boolean = false;
   animationPlayed: boolean = false;
   server: string = 'Devspace';
 
   constructor(private router: Router, public channelService: ChannelService, private userService: UserService, private messageService: MessageService) {
     this.ngOnInit();
-  }
-
-
-  /**
-   * checks the size of the innerwidth of the window
-   * 
-   * @param width the inner width of the browser window
-   */
-  @HostListener('window:resize', ['$event.target.innerWidth'])
-  handleResize(width: number) {
-    if (width < 769) {
-      this.isMobile = true;
-    } else {
-      this.isMobile = false;
-    }
   }
 
   /**
@@ -53,10 +37,7 @@ export class HeaderComponent {
         this.isHomeRoute = event.url.includes('/home');
         this.isLoginRoute = event.url === '/'
         this.isNotLoggedInRoute = ['/register', '/avatar', '/reset-password'].some(route => event.url.includes(route));
-        if (typeof window !== 'undefined'){
-          if (window.innerWidth < 769)
-          setTimeout(() => this.animationPlayed = true, 3000);
-        }
+        setTimeout(() => this.animationPlayed = true, 3000);
       });
   }
 
