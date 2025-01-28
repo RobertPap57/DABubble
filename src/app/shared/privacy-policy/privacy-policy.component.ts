512c55d3c66bcd431aa90423a4f26d2b68db017e
PaterAnderson <DariusMue@web.de>
2025-01-28 13:54:18 +0100

    Finished privacy policy

--- src/app/shared/privacy-policy/privacy-policy.component.ts
+++ src/app/shared/privacy-policy/privacy-policy.component.ts
@@ -1,3 +1,4 @@
+import { Location } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
@@ -8,5 +9,9 @@ import { Component } from '@angular/core';
   styleUrl: './privacy-policy.component.scss'
 })
 export class PrivacyPolicyComponent {
+  constructor(private location: Location) {}
 
+  goBack(): void {
+    this.location.back();
+}
 }
